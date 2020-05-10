30e6bd53750dfc744f062f68197f58558f0c8f59
Finlandiahihto <64765924+Finlandiahihto@users.noreply.github.com>
2020-05-10 13:12:43 +0300

    Add files via upload

---
+++ GitHubJava.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class GitHubJava {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введи возраст");
+        int age = scanner.nextInt();
+        switch (age){
+            case 0:
+                System.out.println("ты родился");
+                break;
+            case 7:
+                System.out.println("ты пошел в школу");
+                break;
+        }
+    }
+}
